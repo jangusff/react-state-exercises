4293b633136c1f02452ef9c818203e2ff6e19a18
Jonathan Angus <jangusff@gmail.com>
2021-06-11 15:57:18 -0500

    working enzyme test of Tabs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import TheDate from './state/TheDate';
-
+import Tabs from './state/Tabs';
 
 /*
 function App() {
@@ -10,8 +9,6 @@ function App() {
     </main>
   );
 }
-*/
-
 
 class App extends Component {
   render() {
@@ -23,6 +20,24 @@ class App extends Component {
     );
   }
 }
+*/
 
+// array of objects each with a name and content
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
+
+class App extends Component {
+  render() {
+    return <div>
+      <Tabs tabs={tabsProp} />
+    </div>
+  }
+}
 
 export default App;
